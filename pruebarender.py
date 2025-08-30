ae4c9d5de3b9937efbadda67425b769243182649
dairovairo <106736572+dairovairo@users.noreply.github.com>
2025-08-30 03:42:58 +0200

    Update pruebarender.py

--- pruebarender.py
+++ pruebarender.py
@@ -5,7 +5,7 @@ import io
 
 app = Flask(__name__)
 
-CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTmbgLgN6Jd460AsuM2NSKwG347DtTQzPiyn-8gGxqWHG0Es69m-mnOFKQmuGZAdw/pub?output=csv"
+CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTmbgLgN6Jd460AsuM2NSKwG347DtTQzPiyn-8gGxqWHG0Es69m-mnOFKQmuGZAdw/pubhtml"
 
 @app.route("/")
 def view_data():
@@ -28,3 +28,4 @@ def view_data():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
