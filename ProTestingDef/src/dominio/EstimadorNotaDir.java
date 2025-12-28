08d35643fdb59599d24062f5d77b0b1988729bc3
dairovairo <106736572+dairovairo@users.noreply.github.com>
2025-12-28 17:12:07 +0100

    Add files via upload

---
+++ ProTestingDef/src/dominio/EstimadorNotaDir.java
@@ -0,0 +1,30 @@
+package dominio;
+
+public class EstimadorNotaDir extends EstimadorNota{
+	 public EstimadorNotaDir(Examen examen) {
+		  super(examen);
+	    }
+	@Override
+	 public double esperanza (int opciones,int ratio) {
+			double puntajeAcierto=(double) (10.0/(double) examen.getPreguntasTotales());
+			double puntajeError= (double) (1.0/ratio);
+			return puntajeAcierto/opciones-((opciones-1)*puntajeError/opciones);
+		}
+	      @Override 
+	 public  double estimarAciertos(double arrayOpciones [],int ratio) {
+			double result=0;
+		  for(int i=0; i<arrayOpciones.length;i++) {
+			  result+=arrayOpciones[i]*esperanza(i+2,ratio);
+
+		  }
+		 
+		  
+		  return result;
+		  
+		}
+	 
+
+
+
+
+}
---
+++ ProTestingDef/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ProTestingDef {
+}
