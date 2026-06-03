a8fb3bab838c7cd23f027b6914c3e2317b4d2d21
DarioGomez11 <Dario.Gomez3@alu.uclm.es>
2026-06-03 20:30:16 +0200

    keloke

--- SocialBattery/sb_koyeb/supabase_schema_phase57_event_promotions.sql
+++
@@ -1,2 +0,0 @@
-alter table community_events add column if not exists promotion_type text not null default 'basic';
-create index if not exists idx_community_events_promotion_type on community_events(promotion_type);
